caa6feaaf1c23c4b049a07845eab8367bc3f7012
Olivier Pichon <op@united-asian.com>
2018-02-11 15:39:49 +0700

    Updated UserMenu component

--- src/components/UserMenu.vue
+++ src/components/UserMenu.vue
@@ -1,27 +1,55 @@
-<template lang="html">
-  <b-nav-item-dropdown :right="right">
-    <template slot="button-content">
-      {{ buttonContent }}
+<template>
+  <b-nav>
+    <template v-if="!isLoggedIn">
+      <b-nav-item href="#" @click="login">
+        {{ $t('user.menu.login') }}
+      </b-nav-item>
+      <b-nav-item href="#" @click="signup">
+        {{ $t('user.menu.signup') }}
+      </b-nav-item>
     </template>
+    <template v-else>
+      <b-nav-item-dropdown :right="right">
+        <template slot="button-content">
+          {{ welcome ?
+              welcome :
+              $t('user.menu.welcome', { user: $uamAuth.user.username  }) }}
+        </template>
 
-    <b-dropdown-item v-if="!noProfile" :to="{ name: profileRoute }">
-      <i class="fa fa-user" aria-hidden="true"></i>&nbsp;
-      {{ $t('label.profile') }}
-    </b-dropdown-item>
+        <b-dropdown-item v-if="!noProfile" :to="{ name: profileRoute }">
+          <i class="fa fa-user" aria-hidden="true"></i>&nbsp;
+          {{ $t('user.menu.profile') }}
+        </b-dropdown-item>
 
-    <slot></slot>
+        <slot></slot>
 
-    <div v-if="!noDivider" class="dropdown-divider"></div>
+        <div v-if="!noDivider" class="dropdown-divider"></div>
 
-    <b-dropdown-item href="#" @click.prevent="logout()">
-      <i class="fa fa-sign-out" aria-hidden="true"></i>&nbsp;
-      {{ $t('label.logout') }}
-    </b-dropdown-item>
-  </b-nav-item-dropdown>
+        <b-dropdown-item href="#" @click.prevent="logout()">
+          <i class="fa fa-sign-out" aria-hidden="true"></i>&nbsp;
+          {{ $t('user.menu.logout') }}
+        </b-dropdown-item>
+      </b-nav-item-dropdown>
+    </template>
+  </b-nav>
 </template>
 
 <script>
 export default {
+  computed: {
+    isLoggedIn () {
+      return this.$uamAuth.isLoggedIn()
+    },
+
+    loginRoute () {
+      return this.$uamAuth.userEndpoints.login
+    },
+
+    signupRoute () {
+      return this.$uamAuth.userEndpoints.signup
+    }
+  },
+
   i18n: {
     messages: {
       'en': require('../translations/user-menu.json')
@@ -29,11 +57,19 @@ export default {
   },
 
   methods: {
+    login () {
+      this.$router.push({ name: this.$uamAuth.userEndpoints.login })
+    },
+
     logout () {
       this.$uamAuth.logout()
         .then(() => {
           this.$router.push({ name: 'login' })
         })
+    },
+
+    signup () {
+      this.$router.push({ name: this.$uamAuth.userEndpoints.login })
     }
   },
 
